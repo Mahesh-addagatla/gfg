5a703de326eded09d0b50083875d132a4ef27df7
Mahesh-addagatla <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-05-06 19:44:07 +0530

    Create PreciseFormat.java

---
+++ PreciseFormat.java
@@ -0,0 +1,12 @@
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.printf("%.7f %.3f",result,result);
+        
+        
+        // Your code here to print upto 3 decimal places
+        
+    }    
+    
+}
