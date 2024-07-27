f8c6b812c50ec87d4dcf03387986e40ac27090c8
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-07-27 23:09:04 +0530

    Create Java Collection | Set 1 (ArrayList) Part-2.java

---
+++ Java Collection | Set 1 (ArrayList) Part-2.java
@@ -0,0 +1,46 @@
+class GfG
+{
+    /*inserts an element x at 
+    the back of the ArrayList A */
+    void add_to_ArrayList(ArrayList<Integer> A, int x)
+    {
+	// Your code here
+	A.add(x);
+    }	
+	
+    /*sort the ArrayList A in ascending order*/
+    void sort_ArrayList_Asc(ArrayList<Integer> A)
+    {
+	// Your code here
+	Collections.sort(A);
+    }
+	
+    /*reverses the ArrayList A*/
+    void reverse_ArrayList(ArrayList<Integer> A)
+    {
+	// Your code here
+	Collections.reverse(A);
+    }
+	
+    /*returns the size of the ArrayList A */
+    int size_Of_ArrayList(ArrayList<Integer> A)
+    {
+	// Your code here
+	return A.size();
+    }
+	
+    /*sorts the ArrayList A in descending order*/
+    void sort_ArrayList_Desc(ArrayList<Integer> A)
+    {
+	// Your code here
+	Collections.sort(A,Collections.reverseOrder());
+    }
+	
+    /*prints space separated 
+    elements of ArrayList A*/
+    void print_ArrayList(ArrayList<Integer> A)
+    {
+        // Your code here
+        for(Integer a:A)System.out.print(a+" ");
+    }
+}
