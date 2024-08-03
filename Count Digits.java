b218adff7e34a0f97273cc6b6a6337e80cf4a787
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-08-03 21:45:23 +0530

    Create Count Digits.java

---
+++ Count Digits.java
@@ -0,0 +1,15 @@
+class Solution{
+    static int evenlyDivides(int N){
+        // code here
+        int c=0;
+        int temp=N;
+        while(temp>0){
+            int r=temp%10;
+            if(r!=0){
+                if(N%r==0)c++;
+            }
+            temp/=10;
+        }
+        return c;
+    }
+}
